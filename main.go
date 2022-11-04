90f539a00df686328ab1ff08d71a2ff70fa7dec0
“Nawi” <nawihusen@gmail.com>
2022-11-04 20:27:38 +0800

    d

--- main.go
+++ main.go
@@ -3,14 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	// var mm []string
-	// fmt.Println(mm)
-	// mm = append(mm, "Baru")
-	// fmt.Println(mm)
-	for _, value := range []int{1, 2, 3, 4, 5, 6, 7, 8, 9} {
-		fmt.Println(value)
-	}
 
+	fmt.Printf("%q\n", `" name \ height "`)
+	// "\" name \\ height \""
 }
 
 // mockery --dir=featur/user(lokasi interface yang akan di mock) --name=Datainterface(nama interface yang di mocking) --filename=UserData.go(nama file hasil generate) --structname=UserData(nama hasil strucr yang di mock)
