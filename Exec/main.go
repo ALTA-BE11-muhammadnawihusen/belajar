edd3706b67418032a6e93f7af2bb7e7df416ac0d
“Nawi” <nawihusen@gmail.com>
2022-11-07 09:26:33 +0800

    +

---
+++ Exec/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"runtime"
+)
+
+func main() {
+	fmt.Println("Exec")
+	test, _ := exec.Command("ls").Output() // menghasilkan []byte
+	fmt.Println(string(test))
+
+	fmt.Println("==================")
+	test2, _ := exec.Command("pwd").Output() // menghasilkan []byte
+	fmt.Println(string(test2))
+
+	fmt.Println("==================")
+	test3, _ := exec.Command("git", "config", "user.name").Output() // bisa gunakan koma untuk pemisah
+	// test4, _ := exec.Command("git config user.name").Output()       //tidak akan menghasilkan apa apa
+	fmt.Println(string(test3))
+	// fmt.Println(string(test4)) // tidak akan menghasilkan apa apa
+
+	t := runtime.GOOS // mengembalikan sistem operasi yang digunakan dalam bentuk string
+	fmt.Println(t)
+}
